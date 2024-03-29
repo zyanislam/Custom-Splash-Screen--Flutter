bae856b7bd0c36b7961f0ac6b2863357d7eeea61
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-29 21:02:02 +0600

    Added Files

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import "package:flutter/material.dart";
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
