aa39401d127eaa63668131ea4e721e0edfba89d6
Zyan <fislam183070@bscse.uiu.ac.bd>
2024-03-30 00:28:32 +0600

    Splash Screen Added

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,7 +1,29 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-class Splash extends StatelessWidget {
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:splash_screen/home.dart';
+
+class Splash extends StatefulWidget {
   const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => MyHome(),
+          ));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
